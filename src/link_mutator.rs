8df249bfbc931992742ca17a363533f8336d375d
JohnnyMorganz <johnnymorganz@outlook.com>
2024-12-24 15:35:44 +0000

    Support re-exporting simple generic defaults
    For now, we only support generic defaults if the
    type was defined earlier as a generic parameter
    
    Fixes #10

--- src/link_mutator.rs
+++ src/link_mutator.rs
@@ -1,7 +1,10 @@
 use anyhow::{bail, Result};
 use full_moon::{
     ast::{
-        luau::{ExportedTypeDeclaration, GenericParameterInfo, IndexedTypeInfo, TypeInfo},
+        luau::{
+            ExportedTypeDeclaration, GenericDeclaration, GenericDeclarationParameter,
+            GenericParameterInfo, IndexedTypeInfo, TypeInfo,
+        },
         punctuated::{Pair, Punctuated},
         span::ContainedSpan,
         Ast, Expression, LastStmt, LocalAssignment, Return, Stmt,
@@ -28,6 +31,31 @@ pub fn type_declarations_from_source(code: &str) -> Result<Vec<ExportedTypeDecla
             _ => None,
         })
         .collect())
+}
+
+fn should_keep_default_type(type_info: &TypeInfo, resolved_types: &[String]) -> bool {
+    // TODO: we could be more clever here, but for now we keep it simple
+    match type_info {
+        TypeInfo::Basic(name) => resolved_types.contains(&name.token().to_string()),
+        TypeInfo::Boolean(_) => true,
+        _ => false,
+    }
+}
+
+fn strip_unknown_default_generics(
+    generics: &GenericDeclaration,
+    resolved_types: &[String],
+) -> Punctuated<GenericDeclarationParameter> {
+    generics
+        .generics()
+        .pairs()
+        .map(|pair| {
+            pair.clone().map(|decl| match decl.default_type() {
+                Some(type_info) if should_keep_default_type(type_info, resolved_types) => decl,
+                _ => decl.with_default(None),
+            })
+        })
+        .collect::<Punctuated<_>>()
 }
 
 pub fn create_new_type_declaration(stmt: &ExportedTypeDeclaration) -> ExportedTypeDeclaration {
@@ -58,16 +86,27 @@ pub fn create_new_type_declaration(stmt: &ExportedTypeDeclaration) -> ExportedTy
         None => IndexedTypeInfo::Basic(stmt.type_declaration().type_name().clone()),
     };
 
-    // Modify the original type declaration to remove the default generics
+    // Modify the original type declaration to remove the default generics, if they are not resolvable
+    let resolved_types = stmt
+        .type_declaration()
+        .generics()
+        .map_or(vec![], |generics| {
+            generics
+                .generics()
+                .iter()
+                .map(|generic| match generic.parameter() {
+                    GenericParameterInfo::Name(name) => name.token().to_string(),
+                    GenericParameterInfo::Variadic { name, .. } => name.token().to_string(),
+                    other => unreachable!("unknown node: {:?}", other),
+                })
+                .collect()
+        });
+
     let original_type_declaration = match stmt.type_declaration().generics() {
         Some(generics) => stmt.type_declaration().clone().with_generics(Some(
-            generics.clone().with_generics(
-                generics
-                    .generics()
-                    .pairs()
-                    .map(|pair| pair.clone().map(|decl| decl.with_default(None)))
-                    .collect::<Punctuated<_>>(),
-            ),
+            generics
+                .clone()
+                .with_generics(strip_unknown_default_generics(generics, &resolved_types)),
         )),
         None => stmt.type_declaration().clone(),
     };
@@ -180,4 +219,45 @@ pub fn mutate_link(
         )
         .with_last_stmt(Some(create_return_require_variable()));
     Ok(MutateLinkResult::Changed(parsed_code.with_nodes(new_nodes)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn re_exports_generic_defaults_if_they_are_part_of_the_type() {
+        let code = r"
+            export type Value<T, S = T> = Types.Value<T, S>
+        ";
+
+        let type_declarations = type_declarations_from_source(code).unwrap();
+        assert_eq!(type_declarations.len(), 1);
+
+        let reexported_type_declarations = re_export_type_declarations(type_declarations);
+        assert_eq!(reexported_type_declarations.len(), 1);
+
+        assert_eq!(
+            reexported_type_declarations[0].0.to_string(),
+            "export type Value<T, S = T> = REQUIRED_MODULE.Value<T, S >"
+        );
+    }
+
+    #[test]
+    fn does_not_re_export_unknown_default_generics() {
+        let code = r"
+            export type Value<T, S = Object> = Types.Value<T, S>
+        ";
+
+        let type_declarations = type_declarations_from_source(code).unwrap();
+        assert_eq!(type_declarations.len(), 1);
+
+        let reexported_type_declarations = re_export_type_declarations(type_declarations);
+        assert_eq!(reexported_type_declarations.len(), 1);
+
+        assert_eq!(
+            reexported_type_declarations[0].0.to_string(),
+            "export type Value<T, S > = REQUIRED_MODULE.Value<T, S >"
+        );
+    }
 }
